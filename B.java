db8493fd4f38b0ccd0837260a7805b4efd56c3ea
sivaprasadm317@gmail.com <sivaprasadm317@gmail.com>
2023-09-24 11:45:31 +0530

    message

--- B.java
+++ B.java
@@ -2,20 +2,41 @@ class B
 {
 	public static void main(String[] args)
 	{
-		int h = 5;
-		for(int i = 0; i < h; i++) {  
-            for(int j = 0; j < h; j++) {  
-                if((i == 0 || i == h / 2 || i == h - 1)) {  
-                    System.out.printf("*");  
-                }else if(i < h / 2 && j == 0){  
-                    System.out.printf("*");  
-                }else if(i > h / 2 && j == h - 1){  
-                    System.out.printf("*");  
-                }else {  
-                    System.out.printf(" ");  
-                }  
-            }  
-            System.out.printf("\n ");  
-        }  
+		 for(int i=1;i<=n;i++)
+    		{
+        		for(int j=i-1;j<=n;j++)
+        		{
+            			System.out.print(" ");
+        		}
+        		for(int j=i;j<=i;j++)
+        		{
+            			System.out.print("* ");
+        		}
+        		System.out.println("");
+    		}
+		for(int i=n;i>=1;i--)
+    		{
+        		for(int j=n;j>=i;j--)
+        		{
+            			System.out.print(" ");
+        		}
+        		for(int j=i;j>=i;j--)
+        		{
+            			System.out.print("* ");
+        		}
+        		System.out.println("");
+    		}
+		for(int i=1;i<=n;i++)
+    		{
+        		for(int j=i-1;j<=n;j++)
+        		{
+            			System.out.print(" ");
+        		}
+        		for(int j=i;j<=i;j++)
+        		{
+            			System.out.print("* ");
+        		}
+        		System.out.println("");
+    		} 
 	}
 }
