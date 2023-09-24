c4ba32868ac66053f620c8fc1a2c49758d683d22
sivaprasadm317@gmail.com <sivaprasadm317@gmail.com>
2023-09-24 11:32:32 +0530

    message

---
+++ A.java
@@ -0,0 +1,20 @@
+class A
+{
+	public static void main(String[] args) 
+	{
+		int i,j,n=5;
+		for(i=1;i<=n;i++)
+    		{
+        		for(j=i;j<=n;j++)
+        		{
+            			System.out.print(" ");
+        		}
+        		for(j=i;j<=n;j++)
+        		{
+            			System.out.print("* ");
+        		}
+        		System.out.println(" ");
+    		}
+		
+	}
+}
